3a9bff9e4cef4039a8d75b4ec2de8c3d4b75389d
Eugene Ware <eugene@noblesamurai.com>
2013-10-05 15:52:25 +1000

    basic writestream support

--- test/index.js
+++ test/index.js
@@ -3,7 +3,12 @@ var expect = require('expect.js'),
     path = require('path'),
     Canvas = require('canvas'),
     concat = require('concat-stream'),
+    stream = require('stream'),
     GIFEncoder = require('..');
+
+function getData(ctx, width, height) {
+  return ctx.getImageData(0, 0, width || ctx.canvas.width, height || ctx.canvas.height).data;
+}
 
 describe('GIFEncoder', function() {
   it('should be able to Generate a PNG', function(done) {
@@ -40,7 +45,7 @@ describe('GIFEncoder', function() {
     done();
   });
 
-  it('should expose a streaming interface', function(done) {
+  it('should expose a read streaming interface', function(done) {
     var buf = fs.readFileSync(path.join(__dirname, 'fixtures', 'in.png'));
     var img = new Canvas.Image();
     img.src = buf;
@@ -72,5 +77,86 @@ describe('GIFEncoder', function() {
     encoder.addFrame(ctx);
 
     encoder.finish();
+  });
+
+  it('should expose a write streaming interface', function(done) {
+    var buf = fs.readFileSync(path.join(__dirname, 'fixtures', 'in.png'));
+    var img = new Canvas.Image();
+    img.src = buf;
+
+    var canvas = new Canvas(img.width, img.height);
+    var ctx = canvas.getContext('2d');
+
+    var encoder = new GIFEncoder(img.width, img.height);
+    encoder.createReadStream().pipe(concat(function (data) {
+      var expected = fs.readFileSync(path.join(__dirname, 'fixtures', 'out.gif'));
+      expect(data).to.eql(expected);
+      done();
+    }));
+
+    encoder.start();
+    encoder.setRepeat(-1);
+    encoder.setDelay(500);
+    encoder.setQuality(10);
+
+    var ws = encoder.createWriteStream();
+
+    ctx.drawImage(img, 0, 0);
+    ws.write(ctx);
+
+    ctx.fillStyle = '#ff0000';
+    ctx.fillRect(0, 0, img.width, img.height);
+    ws.write(ctx);
+
+    ctx.fillStyle = '#0000ff';
+    ctx.fillRect(0, 0, img.width, img.height);
+    ws.write(ctx);
+    ws.end();
+  });
+
+  it('should be able to pipe with bitmaps', function(done) {
+    var rs = new stream.Readable({ objectMode: true });
+    var frames = [];
+    rs._read = function () {
+      if (frames.length) {
+        rs.push(frames.shift());
+      } else {
+        rs.push(null);
+      }
+    };
+
+    var buf = fs.readFileSync(path.join(__dirname, 'fixtures', 'in.png'));
+    var img = new Canvas.Image();
+    img.src = buf;
+
+    var canvas = new Canvas(img.width, img.height);
+    var ctx = canvas.getContext('2d');
+
+    var encoder = new GIFEncoder(img.width, img.height);
+    encoder.createReadStream().pipe(concat(function (data) {
+      var expected = fs.readFileSync(path.join(__dirname, 'fixtures', 'out.gif'));
+      expect(data).to.eql(expected);
+      done();
+    }));
+
+    encoder.start();
+    encoder.setRepeat(-1);
+    encoder.setDelay(500);
+    encoder.setQuality(10);
+
+    var ws = encoder.createWriteStream();
+
+    ctx.drawImage(img, 0, 0);
+    frames.push(getData(ctx));
+
+    ctx.fillStyle = '#ff0000';
+    ctx.fillRect(0, 0, img.width, img.height);
+    frames.push(getData(ctx));
+
+    ctx.fillStyle = '#0000ff';
+    ctx.fillRect(0, 0, img.width, img.height);
+    frames.push(getData(ctx));
+
+    rs.pipe(ws);
   });
 });
