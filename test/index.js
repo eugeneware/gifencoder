9336a4aa18ed2bd049c3fb9a66266d7bad146c54
Eugene Ware <eugene@noblesamurai.com>
2013-10-05 19:31:36 +1000

    streaming only interface

--- test/index.js
+++ test/index.js
@@ -22,40 +22,6 @@ function root(file) {
 }
 
 describe('GIFEncoder', function() {
-  it('should be able to Generate a PNG', function(done) {
-    var buf = fs.readFileSync(fixtures('in.png'));
-    var img = new Canvas.Image();
-    img.src = buf;
-
-    var canvas = new Canvas(img.width, img.height);
-    var ctx = canvas.getContext('2d');
-
-    var encoder = new GIFEncoder(img.width, img.height);
-    encoder.start();
-    encoder.setRepeat(-1);
-    encoder.setDelay(500);
-    encoder.setQuality(10);
-
-    ctx.drawImage(img, 0, 0);
-    encoder.addFrame(ctx);
-
-    ctx.fillStyle = '#ff0000';
-    ctx.fillRect(0, 0, img.width, img.height);
-    encoder.addFrame(ctx);
-
-    ctx.fillStyle = '#0000ff';
-    ctx.fillRect(0, 0, img.width, img.height);
-    encoder.addFrame(ctx);
-
-    encoder.finish();
-    var out = encoder.stream().getData();
-    var expected = fs.readFileSync(fixtures('out.gif'));
-
-    expect(out).to.eql(expected);
-
-    done();
-  });
-
   it('should expose a read streaming interface', function(done) {
     var buf = fs.readFileSync(fixtures('in.png'));
     var img = new Canvas.Image();
